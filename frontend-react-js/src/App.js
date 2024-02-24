cfb133fe48ccf0ee55a50da63c6451dee1498628
Pukhrambam Samir Singh <robinsamir810@gmail.com>
2024-02-24 02:44:50 +0000

    added opentelemetry for distributed tracing

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -14,6 +14,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 
 const router = createBrowserRouter([
   {
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
   {
     path: "/forgot",
     element: <RecoverPage />
+  },
+  {
+    path:"/notifications",
+    element: <NotificationsFeedPage/>
   }
 ]);
 
